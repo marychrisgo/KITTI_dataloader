3464c1ef129b9fef2b9f2762b9e8631f805e00a0
marychrisgo <marychrisgo@yahoo.com.ph>
2022-02-23 12:45:24 +0100

    Edit main.py

--- main.py
+++ main.py
@@ -31,24 +31,3 @@ for i in range(len(KITTI)):
     if i == 3:
         plt.show()
         break
-=======
-import os
-import numpy as np
-import pandas as pd
-import cv2
-import torch
-import matplotlib.image as mpimg
-import matplotlib.pyplot as plt
-from PIL import Image
-from dataloader import KITTI_dataset_handler
-
-KITTI = KITTI_dataset_handler('00')
-
-img = KITTI.left_imgs[0] # how to get the path so I can show it?
-
-img1 = mpimg.imread('./dataset/sequences/00/image_0/' + img)
-plt.imshow(img1)
-plt.show()
-
-
->>>>>>> 442c458437b5603e39aa3ee5c85b81737e91b7d8
